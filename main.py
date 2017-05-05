1d443423a826b0a5d39967b3bdcc7628337b1912
Bokkuto <ludovicvanasse@protonmail.com>
2017-05-04 21:42:43 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+import requests
+import xml.etree.ElementTree as ET
+import os.path
+
+MFP_URL = "http://musicforprogramming.net/rss.php"
+
+DIR_PATH = "/media/Multimedia/Music/MusicForProgramming/"
+
+def get_url(url):
+
+    return requests.get(url).text
+
+def download_url(url):
+
+    return requests.get(url,stream=True)
+
+def load_songs():
+
+    tab_songs = []
+
+    tab_song = {
+        "title" : "",
+        "url"   : ""
+    }
+
+    request_xml = ET.fromstring(get_url(MFP_URL))
+
+    for i in request_xml:
+
+        for j in i:
+
+            if j.tag == "item":
+
+                for k in j :
+
+                    if k.tag == "comments":
+
+                        tab_song['title'] = k.title
+
+                        tab_song['url'] = k.comments
+
+                        tab_songs.append(tab_song)
+
+    return tab_songs
+
+def download_songs():
+
+    tab_songs = load_songs()
+
+    for song in tab_songs:
+
+        ## Download the song and store it into the variable
+        song_music = download_url(song['url'])
+
+        if not os.path.isfile(DIR_PATH + song['title']):
+
+            with open("{}".format(song['title']),"wb") as file :
+
+                for chunk in song_music.iter_content(chunk_size = 1024):
+
+                    file.write(chunk)
+
+
+def __main__():
+
+    download_songs()
+
+
+if __name__ :
+
+    __main__()
