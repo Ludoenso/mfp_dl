90720da82481080210861e8838bdf0ea3e5eea8f
Bokkuto <ludovicvanasse@protonmail.com>
2017-07-01 14:49:25 -0400

    Rename the files and add some basic info for the song class

---
+++ lib/song.py
@@ -0,0 +1,12 @@
+# Todo :
+# - Dynamicly fix title name for file system
+
+class song(object):
+
+    __init__(self,url,name):
+
+        self.url = url
+
+        self.name = self.convert_name()
+
+    def convert_name(self)
