39f5998e446bcfaafc8aa4e3c5c3f72d07aaa621
Ludoenso <ludovicvanasse@gmail.com>
2017-12-30 16:08:50 +0000

    Added arguments support for the url and the directory.

--- bin/rss_dl.py
+++ bin/rss_dl.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-#Todo :
+# Todo :
 # - Make is download any RSS feed
 # - Dynamicly fix title name for file system
 # - Be able to add arguments to set the recurence of the download
@@ -7,25 +7,21 @@
 # - Find a way to sync it to a device e.g. Phone
 
 
-
 import requests
 import xml.etree.ElementTree as ET
 import os.path
-import filecmp
 import argparse
 
 URL = ""
-# URL = "http://musicforprogramming.net/rss.php"
 
 XML = ""
 
 DIR_PATH = ""
 
-# DIR_PATH = "/media/Multimedia/Music/MusicForProgramming/"
-
 tab_song_url = []
 
 tab_song_title = []
+
 
 def get_argv():
 
@@ -33,18 +29,22 @@ def get_argv():
     parser = argparse.ArgumentParser(description="""Specify the URL and the path
                                                 directory""")
 
-    parser.add_argument('directory', metavar="DIR_PATH", type = string, nargs=1)
+    parser.add_argument('--dir', nargs=1, help="""the path to the directory 
+        where you saved the rss file you want to download.""")
+
+    parser.add_argument('--url', help="the URL that you want to download.")
+
+    return parser.parse_args()
 
 
 def get_url2xml(url):
 
-    print(url)
-
     return ET.fromstring(requests.get(url).text)
+
 
 def download_url(url):
 
-    return requests.get(url,stream=True)
+    return requests.get(url, stream=True)
 
 
 # Append the url of the song into the tab_song
@@ -59,14 +59,16 @@ def load_song_url(xml):
 
             if j.tag == "item":
 
-                for k in j :
+                for k in j:
 
                     if k.tag == "comments":
-
+                        
                         tab_song_url.append(k.text)
 
 # Append the title of the song into the tab_song
 # dictionnary
+
+
 def load_song_name(xml):
 
     for i in xml:
@@ -75,39 +77,43 @@ def load_song_name(xml):
 
             if j.tag == "item":
 
-                for k in j :
+                for k in j:
 
                     if k.tag == "title":
 
-                        string = k.text.replace(" ","_")
+                        song_name_file = k.text.replace(" ", "_")
 
-                        string = string.replace(":","")
+                        song_name_file = song_name_file.replace(":", "")
 
-                        string += ".mp3"
+                        song_name_file += ".mp3"
 
-                        tab_song_title.append(string)
+                        tab_song_title.append(song_name_file)
 
 
 # If the name of the song is not in the directory download it.
-def download_songs():
+def download_songs(DIR_PATH):
 
-    for i in range(0,len(tab_song_url)):
+    for i in range(0, len(tab_song_url)):
 
-        ## Download the song and store it into the variable
+        # Download the song and store it into the variable
         song_file = download_url(tab_song_url[i])
 
         if not os.path.isfile(DIR_PATH + tab_song_title[i]):
 
-            with open(DIR_PATH + "{}".format(tab_song_title[i]),"wb") as file :
+            with open(DIR_PATH + "{}".format(tab_song_title[i]), "wb") as file:
 
-                for chunk in song_file.iter_content(chunk_size = 1024):
+                for chunk in song_file.iter_content(chunk_size=1024):
 
                     file.write(chunk)
+
+
 def __main__():
 
-    get_argv()
+    args = get_argv()
 
-    print("Printing URL : " + URL)
+    DIR_PATH = args.dir[0]
+
+    URL = args.url
 
     XML = get_url2xml(URL)
 
@@ -115,9 +121,9 @@ def __main__():
 
     load_song_name(XML)
 
-    # download_songs()
+    download_songs(DIR_PATH)
 
 
-if __name__ :
+if __name__:
 
     __main__()
